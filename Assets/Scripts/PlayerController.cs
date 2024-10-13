dbcf44b8e3c25b71a8fa7f69f2d4d61c87372921
Githiora <gabriel.wamunyu@iconicmoments.co>
2024-10-14 01:41:42 +0300

    Constrain the Player’s movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private float speed = 5.0f;
+    private Rigidbody playerRb;
+    private float zBoundTop = 6;
+    private float zBoundBottom = -4.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+
+        if (transform.position.z > zBoundTop)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBoundTop);
+        }
+
+        if (transform.position.z < zBoundBottom)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBoundBottom);
+        }
     }
 }
